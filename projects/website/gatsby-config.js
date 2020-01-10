36dd955397bb740932b870d1447999873a737d3e
Brekk <brekk@brekkbockrath.com>
2020-01-10 20:58:16 +0100

    splitting out other post types

--- projects/website/gatsby-config.js
+++ projects/website/gatsby-config.js
@@ -1,3 +1,7 @@
+const R = require('ramda')
+
+const { map, curry, pipe, reduce, merge } = R
+
 const gatsbyRemarkPlugins = (process.env.OFFLINE
   ? []
   : [
@@ -12,6 +16,157 @@ const gatsbyRemarkPlugins = (process.env.OFFLINE
     showLineNumbers: true
   }
 })
+
+const srcFs = xx =>
+  Array.isArray(xx) && xx[1]
+    ? {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: xx[0],
+          path: `${__dirname}/src/${xx[1]}/`
+        }
+      }
+    : {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: xx,
+          path: `${__dirname}/src/${xx}/`
+        }
+      }
+
+const KEYED_PATHS = [
+  ['glossary', 'content/glossary'],
+  ['modules', 'content/modules'],
+  'pages',
+  'posts',
+  'routes',
+  ['reviews', 'content/reviews']
+]
+
+const namedFilePaths = map(srcFs, KEYED_PATHS)
+
+const layout = curry((pt, kk) =>
+  Array.isArray(kk) ? { [kk[0]]: require.resolve(pt) } : { [kk]: require.resolve(pt) }
+)
+const allLayoutsAreTheSame = layout('./src/templates/MDXPage/index.js')
+const defaultLayouts = pipe(map(allLayoutsAreTheSame), reduce(merge, {}))(KEYED_PATHS)
+
+const plugins = (process.env.OFFLINE
+  ? []
+  : [
+      {
+        resolve: 'gatsby-source-meetup-events',
+        options: {
+          groupId: `open-sorcerers`
+        }
+      },
+      {
+        resolve: `gatsby-source-github-contributors`,
+        options: {
+          repo: 'http://github.com/open-sorcerers/open-sorcerers'
+        }
+      }
+    ]
+).concat([
+  {
+    resolve: `gatsby-plugin-build-date`,
+    options: {
+      formatting: {
+        format: 'DD-MM-YYYY',
+        utc: true
+      }
+    }
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        /* {
+            resolve: `gatsby-remark-embed-snippet`,
+            options: {}
+          }, */
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            showLineNumbers: true
+          }
+        }
+      ]
+    }
+  },
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-emotion',
+  ...namedFilePaths,
+  {
+    resolve: 'gatsby-plugin-mdx',
+    options: {
+      extensions: ['.mdx', '.md'],
+      defaultLayouts,
+      remarkPlugins: [{ resolve: 'remark-slug' }, { resolve: 'remark-autolink-headings' }],
+      gatsbyRemarkPlugins
+    }
+  },
+
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'images',
+      path: `${__dirname}/src/assets`
+    }
+  },
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      name: 'gatsby-starter-default',
+      short_name: 'starter',
+      start_url: '/',
+      background_color: '#440099',
+      theme_color: '#440099',
+      display: 'minimal-ui',
+      icon: 'src/assets/logo-open-sorcerers.png' // This path is relative to the root of the site
+    }
+  },
+  {
+    resolve: `gatsby-plugin-react-svg`,
+    options: {
+      rule: {
+        include: /assets/
+      }
+    }
+  },
+
+  {
+    resolve: `gatsby-plugin-alias-imports`,
+    options: {
+      alias: {
+        '@root': '.',
+
+        '@src': 'src',
+
+        '@assets': 'src/assets',
+        '@components': 'src/components',
+        '@constants': 'src/constants',
+        '@domain': 'src/domain',
+        '@pages': 'src/pages',
+        '@posts': 'src/posts',
+        '@queries': 'src/queries',
+        '@routes': 'src/routes',
+        '@services': 'src/services',
+        '@styles': 'src/styles',
+        '@templates': 'src/templates',
+        '@utils': 'src/utils',
+
+        '@content': 'src/content',
+        '@glossary': 'src/content/glossary',
+        '@modules': 'src/content/modules',
+        '@reviews': 'src/content/reviews'
+      },
+      extensions: ['js', 'mdx']
+    }
+  }
+])
 
 module.exports = {
   siteMetadata: {
@@ -33,145 +188,36 @@ module.exports = {
     type: 'website',
     image: 'https://open.sorcerers.dev/logo-open-sorcerers.png'
   },
-  plugins: [
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          /* {
-            resolve: `gatsby-remark-embed-snippet`,
-            options: {}
-          }, */
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              showLineNumbers: true
-            }
-          }
-        ]
-      }
-    },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-emotion',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: `${__dirname}/src/pages/`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/src/posts/`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'routes',
-        path: `${__dirname}/src/routes/`
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: ['.mdx', '.md'],
-        defaultLayouts: {
-          routes: require.resolve('./src/templates/MDXPage/index.js'),
-          posts: require.resolve('./src/templates/MDXPage/index.js')
-        },
-        remarkPlugins: [{ resolve: 'remark-slug' }, { resolve: 'remark-autolink-headings' }],
-        gatsbyRemarkPlugins
-      }
-    },
+  plugins
+  // The following are all optional plugins that you may find useful.
+  // If you choose not to use them, consider removing them from your package.json!
 
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/assets`
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#440099',
-        theme_color: '#440099',
-        display: 'minimal-ui',
-        icon: 'src/assets/logo-open-sorcerers.png' // This path is relative to the root of the site
-      }
-    },
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-        rule: {
-          include: /assets/
-        }
-      }
-    },
+  // This plugin enables Progressive Web App + Offline functionality
+  // https://gatsby.app/offline
+  // 'gatsby-plugin-offline',
 
-    {
-      resolve: `gatsby-plugin-alias-imports`,
-      options: {
-        alias: {
-          '@root': '.',
+  // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
+  // {
+  //   resolve: 'gatsby-plugin-google-analytics',
+  //   options: {
+  //     trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+  //     head: false,
+  //     anonymize: false,
+  //     respectDNT: false,
+  //   },
+  // },
 
-          '@src': 'src',
+  // (optional) Enable the following for Google tag manager
+  // https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/
+  // {
+  //   resolve: 'gatsby-plugin-google-tagmanager',
+  //   options: {
+  //     id: 'YOUR_GOOGLE_TAGMANAGER_ID',
+  //     defaultDataLayer: { platform: "gatsby" },
+  //   },
+  // },
 
-          '@assets': 'src/assets',
-          '@components': 'src/components',
-          '@constants': 'src/constants',
-          '@domain': 'src/domain',
-          '@pages': 'src/pages',
-          '@posts': 'src/posts',
-          '@queries': 'src/queries',
-          '@routes': 'src/routes',
-          '@services': 'src/services',
-          '@styles': 'src/styles',
-          '@templates': 'src/templates',
-          '@utils': 'src/utils'
-        },
-        extensions: ['js', 'mdx']
-      }
-    }
-
-    // The following are all optional plugins that you may find useful.
-    // If you choose not to use them, consider removing them from your package.json!
-
-    // This plugin enables Progressive Web App + Offline functionality
-    // https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-
-    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
-    // {
-    //   resolve: 'gatsby-plugin-google-analytics',
-    //   options: {
-    //     trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
-    //     head: false,
-    //     anonymize: false,
-    //     respectDNT: false,
-    //   },
-    // },
-
-    // (optional) Enable the following for Google tag manager
-    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/
-    // {
-    //   resolve: 'gatsby-plugin-google-tagmanager',
-    //   options: {
-    //     id: 'YOUR_GOOGLE_TAGMANAGER_ID',
-    //     defaultDataLayer: { platform: "gatsby" },
-    //   },
-    // },
-
-    // (optional) Enable the following for generation of an XML sitemap
-    // https://www.gatsbyjs.org/packages/gatsby-plugin-advanced-sitemap/
-    // 'gatsby-plugin-advanced-sitemap',
-  ]
+  // (optional) Enable the following for generation of an XML sitemap
+  // https://www.gatsbyjs.org/packages/gatsby-plugin-advanced-sitemap/
+  // 'gatsby-plugin-advanced-sitemap',
 }

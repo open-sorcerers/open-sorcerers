46d30e4f3fa2096bb8e9b5ed8beea885ed543300
Brekk <brekk@brekkbockrath.com>
2020-01-20 17:14:31 +0100

    contributors was all fake n shit

--- projects/website/src/components/Contributors/Contributors.js
+++ projects/website/src/components/Contributors/Contributors.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { css } from '@emotion/core'
-import { concat, prop, pipe, pathOr, map } from 'ramda'
+import { prop, pipe, pathOr, map } from 'ramda'
 import { Badge } from '@components/Badge'
 
 import { getContributors } from '@queries/contributors'
@@ -45,7 +45,7 @@ export const Contributors = () => {
       <ul>
         {pipe(
           pathOr([], ['allGitHubContributor', 'nodes']),
-          //*
+          /*
           concat([
             {
               id: 'fake',

bf2d7bc9e25f91035b5f8075e49235057a7c59db
Brekk <brekk@brekkbockrath.com>
2020-01-25 22:10:14 +0100

    rewrite that idea with gripgrep tomorrow

--- projects/website/scripts/update-date-edited.js
+++ projects/website/scripts/update-date-edited.js
@@ -1,8 +1,12 @@
 const {
+  equals,
+  cond,
   last,
   curry,
   filter,
   map,
+  T,
+  always: K,
   identity: I,
   reject,
   includes,
@@ -27,6 +31,24 @@ const readStdin = () =>
 const C = { n: '\n', _: ' ' }
 const smooth = filter(I)
 
+const readableMonth = cond([
+  [equals('Jan'), K('01')],
+  [equals('Feb'), K('02')],
+  [equals('Mar'), K('03')],
+  [equals('Apr'), K('04')],
+  [equals('May'), K('05')],
+  [equals('Jun'), K('06')],
+  [equals('Jul'), K('07')],
+  [equals('Aug'), K('08')],
+  [equals('Sep'), K('09')],
+  [equals('Oct'), K('10')],
+  [equals('Nov'), K('11')],
+  [equals('Dec'), K('12')],
+  [T, K('???')]
+])
+
+const readableDay = z => (z < 10 ? '0' + z : '' + z)
+
 module.exports = pipe(
   readStdin,
   map(
@@ -42,10 +64,18 @@ module.exports = pipe(
       map(slice(-6, Infinity)),
       map(([z, m, d, t, y, f]) => {
         const mm = m === '' ? z : m
+        const ff = {
+          m: mm,
+          d: parseInt(d),
+          /* t: pipe(split(':'), map(parseInt))(t), */
+          t,
+          y: parseInt(y)
+        }
+        const formatted = `${ff.y}-${readableMonth(ff.m)}-${readableDay(ff.d)}T${t}.000Z`
         return typeof t !== 'string'
           ? {}
           : {
-              [f]: { m: mm, d: parseInt(d), t: pipe(split(':'), map(parseInt))(t), y: parseInt(y) }
+              [f]: `dateEdited: ` + formatted
             }
       }),
       reduce(mergeRight, {})

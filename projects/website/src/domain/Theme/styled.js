2f799921032fd393fea1c2e8caee2e9cca914243
Brekk <brekk@brekkbockrath.com>
2020-01-29 22:24:03 +0100

    it runs

--- projects/website/src/domain/Theme/styled.js
+++ projects/website/src/domain/Theme/styled.js
@@ -1,27 +1,43 @@
 import styled from '@emotion/styled'
-import { Box } from 'rebass'
+import { Box, Button } from 'rebass'
+import { Z_INDEX } from '@styles/constants'
 import { pathOr } from 'ramda'
 
 const grab = pathOr('lime')
 
 const picker = grab(['theme', 'colors', 'ui', 'picker', 'f'])
+const pickerE = grab(['theme', 'colors', 'cs', 'picker', 'b'])
 const pickerB = grab(['theme', 'colors', 'ui', 'picker', 'b'])
 const pickerA = grab(['theme', 'colors', 'ui', 'picker', 'a', 'f'])
-const pickerAF = grab(['theme', 'colors', 'ui', 'picker', 'a', 'f'])
+const pickerAB = grab(['theme', 'colors', 'ui', 'picker', 'a', 'b'])
 export const StyledPicker = styled(Box)`
   font-size: 1.5rem;
   display: flex;
   flex-direction: row;
   width: 8rem;
-  height: 1rem;
-  line-height: 1rem;
+  height: 3rem;
+  line-height: 3rem;
   color: ${picker};
-  background-color: ${pickerB};
+  background-color: ${pickerE};
   position: fixed;
   bottom: 0.5rem;
   left: 0.5rem;
+  border-radius: 100rem;
+  justify-content: center;
+  z-index: ${Z_INDEX.FLOATING};
+  align-items: center;
 `
 
-export const Next = styled(Box)``
-export const Prev = styled(Box)``
-export const Shuffle = styled(Box)``
+export const PickerButton = styled(Box)`
+  width: 1.5rem;
+  height: 1.5rem;
+  line-height: 1.5rem;
+  text-align: center;
+  color: ${picker};
+  background-color: ${pickerB};
+  border-radius: 100rem;
+  &:hover {
+    color: ${pickerA};
+    background-color: ${pickerAB};
+  }
+`

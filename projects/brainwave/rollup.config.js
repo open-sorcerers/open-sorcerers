f6240a1ae7344cae4fb0420d83e7435e5e45c030
Brekk <brekk@brekkbockrath.com>
2020-02-02 19:20:47 +0100

    brainwave2

---
+++ projects/brainwave/rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from "@rollup/plugin-node-resolve"
+import cjs from "@rollup/plugin-commonjs"
+import cli from "rollup-plugin-cli"
+import buble from "@rollup/plugin-buble"
+import json from "@rollup/plugin-json"
+import progress from "rollup-plugin-progress"
+/* import sizes from "rollup-plugin-sizes" */
+import pkg from "./package.json"
+
+const external = pkg && pkg.dependencies ? Object.keys(pkg.dependencies) : []
+const plugins = [
+  progress(),
+  json(),
+  cjs({
+    extensions: [`.js`, `.json`],
+    include: `node_modules/**`
+  }),
+  resolve({
+    preferBuiltins: true
+  }),
+  buble({
+    transforms: { forOf: false },
+    objectAssign: true
+  })
+  /* sizes({ details: true }) */
+]
+
+export default [
+  {
+    input: `src/index.js`,
+    external,
+    output: [{ file: pkg.main, format: `cjs` }],
+    plugins
+  },
+  {
+    input: `src/cli-runner.js`,
+    external: external.concat(["fs"]),
+    output: [{ file: `engraved-cli.js`, format: `cjs` }],
+    plugins: plugins
+      .slice(0, 2)
+      .concat([cli()])
+      .concat(plugins.slice(2, Infinity))
+  }
+]

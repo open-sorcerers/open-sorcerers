c3c0942a1b7bc7c6480037d245d83dee7658f07f
Brekk <brekk@brekkbockrath.com>
2020-02-04 10:16:57 +0100

    running and gunning

---
+++ projects/brainwave/brainwave.config.js
@@ -0,0 +1,62 @@
+const R = require("ramda")
+const compareAsc = require("date-fns/fp/compareAsc")
+
+const timeAfterTime = R.curry((a, b) => compareAsc(a, b) > -1)
+
+// brainwave (TM) output
+/*
+const BRAINWAVE_NETWORK  = {
+  files: [], // list of filenames
+  keys: [], // union of all frontmatter keys across all files
+  waves: [
+    {stats, filename, keys, brain}
+  ]
+}
+*/
+
+const getFiles = R.propOr([], "files")
+const getKeys = R.propOr([], "keys")
+const hasKey = R.curry((key, wave) => R.pipe(getKeys, R.includes(key))(wave))
+const getWaves = R.propOr([], "waves")
+
+module.exports = () => ({
+  // mind control mutates data in your frontmatter
+  // each key is an array of [condition, mutation]
+  // where condition is a unary function which receives each wave as its only parameter
+  // and mutation is also a unary function which receives each wave as its only parameter
+  // if mutation returns a non-null result, that value will be merged with the previous state
+  // (using mergeRight)
+  control: {
+    publishingEdited: [
+      /* condition */
+      () => true,
+      /* mutation */
+      R.pipe(
+        R.pathOr(-1, ["stats", "ctime"]),
+        z => z.substr(0, z.indexOf("T")),
+        R.assoc("dateEdited")
+      )
+    ],
+    publishingPublished: [
+      /* condition */
+      R.both(
+        // datePublished >= now
+        R.pipe(
+          R.pathOr(-1, ["brain", "datePublished"]),
+          x => new Date(x),
+          // test this
+          timeAfterTime(new Date(Date.now()))
+        ),
+        // is draft
+        R.pathOr(false, ["brain", "draft"])
+      ),
+      /* mutation */
+      R.assoc("draft", false)
+    ]
+  },
+  // telepathy queries your frontmatter, using basically the same interface as mind control, but simplified
+  // to only be the condition part of the mind control above, so no array wrapping either
+  telepathy: {
+    learnPages: R.propEq("menu", "learn")
+  }
+})
--- projects/brainwave/src/brainwave.js
+++ projects/brainwave/src/brainwave.js
@@ -1,7 +1,20 @@
 import path from "path"
 import { stat as rawStat } from "torpor"
-import { ifElse, __ as $, pipe, propOr, curryN, curry, map } from "ramda"
-import { reject } from "fluture"
+import {
+  reject,
+  is,
+  assoc,
+  chain,
+  ifElse,
+  __ as $,
+  pipe,
+  propOr,
+  curryN,
+  curry,
+  map
+} from "ramda"
+import { parallel, reject as bad } from "fluture"
+import { trace } from "xtrace"
 import { cosmiconfig as cosmic } from "cosmiconfig"
 import { futurizeWithCancel } from "ensorcel"
 
@@ -9,16 +22,25 @@ import { glob } from "./utils"
 
 export const relativePath = curryN(2, path.resolve)
 
-export const stat = rawStat($, { bigint: false })
+export const stat = x =>
+  pipe(
+    rawStat($, { bigint: false }),
+    map(reject(is(Function))),
+    map(assoc("name", x))
+  )(x)
 
 export const findBrainsRelativeTo = curry((fileTypes, basePath) =>
-  pipe(relativePath(basePath), glob, map(stat))("/**/*." + fileTypes)
+  pipe(
+    trace("where"),
+    glob,
+    chain(list => parallel(10)(map(stat, list)))
+  )(basePath + "/**/*." + fileTypes)
 )
 
 export const getBasePath = x => propOr(process.cwd(), "root", x)
 
 export const brainwave = curry((config, file) => {
-  const cc = cosmic()
+  const cc = cosmic("brainwave")
   let isCancelled = false
   const cancel = () => {
     isCancelled = true
@@ -29,11 +51,39 @@ export const brainwave = curry((config, file) => {
   const loadOrSearch = c => (c ? load(c) : search())
   const xavier = ifElse(
     () => isCancelled,
-    () => reject("Is cancelled!"),
+    () => bad(new Error("Is cancelled!")),
     pipe(
       // look for --config / -c flag
       propOr(false, "config"),
-      loadOrSearch
+      loadOrSearch,
+      // think about injecting some props here so that the user doesn't have to import their own stuff?
+      map(({ config: cnf }) => {
+        if (cnf) {
+          const memoryPalace = cnf()
+          if (
+            !memoryPalace ||
+            (!memoryPalace.control && !memoryPalace.telepathy)
+          ) {
+            bad(
+              new Error(
+                "Expected to have brainwave config have one or more keys in: [telepathy, control]"
+              )
+            )
+          }
+          return memoryPalace
+        }
+        bad(new Error("Expected to have brainwave config return a function!"))
+      }),
+      chain(({ control, telepathy }) => {
+        return map(
+          brains => ({ brains, control, telepathy }),
+          findBrainsRelativeTo(
+            config.fileTypes || "{md,mdx}",
+            config.basePath || process.cwd()
+          )
+        )
+      })
+
       // map(data)
     )
   )

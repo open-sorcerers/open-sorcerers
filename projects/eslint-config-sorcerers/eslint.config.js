68e0ec8bc37b7a0fc2eab014b827a118c56c19e0
Brekk <brekk@brekkbockrath.com>
2020-06-28 12:59:46 +0200

    try this

--- projects/eslint-config-sorcerers/eslint.config.js
+++ projects/eslint-config-sorcerers/eslint.config.js
@@ -30,34 +30,9 @@ module.exports = {
       {
         json: "always",
         js: "never",
-        validation: "always",
-        task: "always"
       }
     ],
-    "jsdoc/check-alignment": 1,
-    "jsdoc/check-examples": 1,
-    "jsdoc/check-indentation": 1,
-    "jsdoc/check-param-names": 1,
-    "jsdoc/check-syntax": 1,
-    "jsdoc/check-tag-names": 1,
-    "jsdoc/implements-on-classes": 1,
-    "jsdoc/match-description": 1,
-    "jsdoc/newline-after-description": 1,
-    "jsdoc/no-undefined-types": 1,
-    "jsdoc/require-description": 1,
-    "jsdoc/require-example": 1,
-    "jsdoc/require-hyphen-before-param-description": 1,
-    "jsdoc/require-jsdoc": 1,
-    "jsdoc/require-param": 1,
-    "jsdoc/require-param-description": 1,
-    "jsdoc/require-param-name": 1,
-    "jsdoc/require-param-type": 1,
-    "jsdoc/require-returns": 1,
-    "jsdoc/require-returns-check": 1,
-    "jsdoc/require-returns-description": 1,
-    "jsdoc/require-returns-type": 1,
-    "jsdoc/valid-types": 1
   },
   extends: ["standard", "prettier"],
-  plugins: ["babel", "fp", "import", "prettier", "jsdoc", "ramda"]
+  plugins: ["babel", "fp", "import", "prettier", "ramda"]
 }

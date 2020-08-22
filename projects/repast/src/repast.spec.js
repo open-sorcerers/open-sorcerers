881db20a42e52d8db6d1c291d895c6dd030d1656
Brekk <brekk@brekkbockrath.com>
2020-08-22 20:17:42 +0200

    more robustification

--- projects/repast/src/repast.spec.js
+++ projects/repast/src/repast.spec.js
@@ -1,15 +1,25 @@
 import { readFile } from "torpor"
-import { __, pipe, map, prop } from "ramda"
+import { dissoc, __, pipe, map, propEq, prop, ifElse } from "ramda"
 import { fork } from "fluture"
-import { parseWithConfig } from "./parser"
 import { parse } from "recast"
+import { parseWithConfig } from "./parser"
+import { TYPES } from "./constants"
 
 test("basic", done => {
   pipe(
     readFile(__, "utf8"),
     map(parseWithConfig({})),
     fork(done)(outcome => {
-      expect(map(prop("signature"), outcome)).toMatchSnapshot()
+      expect(
+        map(
+          ifElse(
+            propEq("type", TYPES.HM_SIGNATURE),
+            prop("signature"),
+            dissoc("ast")
+          ),
+          outcome
+        )
+      ).toMatchSnapshot()
       done()
     })
   )("./fixture/example.js")

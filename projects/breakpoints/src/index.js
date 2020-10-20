a0a468f9d138f2542ec333338ec0d3f812f3d16c
Brekk <brekk@brekkbockrath.com>
2020-10-20 06:09:46 +0200

    breakpoints2

--- projects/breakpoints/src/index.js
+++ projects/breakpoints/src/index.js
@@ -3,13 +3,17 @@ import React from "react"
 import styled from "@emotion/styled"
 import { map, toPairs, pipe, propOr, replace } from "ramda"
 
+const size = propOr(0, 'size')
+const limeDefault = propOr('lime', 'color')
+const content = pipe(propOr(false, "label"), replace(/_/g, " "))
+
 export const Breakpoint = styled.div`
   position: fixed;
   height: 100vh;
   width: 1rem;
   z-index: 100;
   top: 0;
-  left: ${pipe(propOr(0, "size"))};
+  left: ${size};
   border-left: 1px dashed lime;
   opacity: 0.1;
   cursor: crosshair;
@@ -18,14 +22,39 @@ export const Breakpoint = styled.div`
   }
   &::before {
     position: absolute;
-    background-color: lime;
+    background-color: ${limeDefault};
     color: black; 
-    content: "${pipe(propOr(false, "label"), replace(/_/g, " "))}";
+    content: "${content}";
     transform: rotate(-90deg);
     padding: 0 3rem 0 1rem;
     width: 10rem;
     margin-left: -6.25rem;
     margin-top: 2rem;
+  }
+`
+
+export const HorizontalBreakpoint = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 1rem;
+  z-index: 100;
+  left: 0;
+  top: ${size};
+  border-bottom: 1px dashed lime;
+  opacity: 0.1;
+  cursor: crosshair;
+  &:hover {
+    opacity: 1;
+  }
+  &::before {
+    position: absolute;
+    background-color: ${limeDefault};
+    color: black; 
+    content: "${content}";
+    padding: 0 3rem 0 1rem;
+    width: 10rem;
+    left: 0;
+    margin-top: -0.5rem;
   }
 `
 
